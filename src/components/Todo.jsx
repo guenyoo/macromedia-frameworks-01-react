98e29ac9c961f4a0cb37def7b6d9d03b24db483a
Örsan <guenay@gmail.com>
2024-03-18 10:51:13 +0100

    Add Example ToDo w/ Icon

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,19 @@
+import { ArchiveBoxXMarkIcon } from '@heroicons/react/24/outline';
+
 const Todo = () => {
-  return <div>ToDo Component</div>;
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold text-red">ToDo Component</h2>
+      <div className="grid grid-cols-3 grid-flow-col auto-cols-max border border-slate-300 rounded p-3">
+        <input type="checkbox" id="ToDo1" className="mr-auto" />
+        <label htmlFor="ToDo1">ToDo1</label>
+        <ArchiveBoxXMarkIcon
+          onClick={console.log}
+          className="w-6 h-6 ml-auto"
+        />
+      </div>
+    </div>
+  );
 };
 
 export { Todo };
