771f9605976a4a6b9813aea8c387541d525b6bb7
Örsan <guenay@gmail.com>
2024-03-18 10:08:44 +0100

    Use Todo Component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from 'react'
+import { Todo } from './components/Todo';
 
 function App() {
-  const [text, setText] = useState('This is the original Text');
-  const changeColorAndText = () => {
-    setText('The text has changed!');
-  }
-
   return (
     <main className="grid place-items-center place-content-center h-screen">
-      <p className={text.includes('original') ? 'text-slate-900' : 'text-red-600'}>
-        {text}
-      </p>
-      <button
-        className="bg-teal-100 p-2 rounded border border-blue-300 font-semibold text-teal-700 shadow"
-        onClick={changeColorAndText}
-      >
-        Change Text and Color
-      </button>
+      <Todo />
     </main>
   )
 }
