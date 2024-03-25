01a7347280f60a6a26707792b9654def83285aad
Örsan <guenay@gmail.com>
2024-03-25 09:16:09 +0100

    Export useTodos to hooks and import it in TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,30 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
 import { Todo } from './Todo';
-import { useState } from 'react';
+import { useTodos } from './Todolist.hooks';
 
 const TodoList = ({ todos }) => {
-  // Der useState Hook ist die Möglichkeit von React, auf Variablen
-  // und Veränderungen zu hören und den Komponentenbaum neu zu rendern
-  // bei Änderungen an besagtem State
-  // das in Klammern ist immer der initial zu vergebende Wert,
-  // in diesem Fall also das initiale Todo Array aus den props
-  const [stateTodos, setStateTodos] = useState(todos);
-
-  // Hier separieren wir nur noch nach bereits abgeschlossenen
-  // und noch offenen Todos
-  const openTodos = stateTodos.filter((todo) => !todo.completed);
-  const completedTodos = stateTodos.filter((todo) => todo.completed);
-
-  const changeTodoState = (id) => {
-    const todosToChange = stateTodos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, completed: !todo.completed };
-      }
-      return todo;
-    });
-    setStateTodos(todosToChange);
-  };
+  const [openTodos, completedTodos, changeTodoState] = useTodos(todos);
 
   return (
     <>
@@ -42,7 +22,7 @@ const TodoList = ({ todos }) => {
       </section>
 
       <section className="grid gap-3">
-        <h3 className="text-xl ">Completed ToDo's</h3>
+        <h3 className="text-xl">Completed ToDo's</h3>
         {completedTodos.map((todo) => (
           <Todo
             key={todo.id}
