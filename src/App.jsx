df191e771b32d83ae32d261e49eaee1ca6b22c26
guenyoo <guenay@gmail.com>
2024-04-25 14:09:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Todos } from './components/Todos';
 function App() {
   return (
     <main className="grid place-items-center place-content-center h-screen">
+      <h1>Hallo Marc!</h1>
       <Todos />
     </main>
   );
